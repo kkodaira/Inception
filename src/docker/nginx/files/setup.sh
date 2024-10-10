571248a099e2cbdc6a2decdcfc5f06c520deb6b3
Kaistuc <kodaira-kai-vk@ynu.jp>
2024-10-10 18:06:38 +0900

    設定ファイル見直し

--- src/docker/nginx/files/setup.sh
+++ src/docker/nginx/files/setup.sh
@@ -23,7 +23,7 @@ echo '
 	
 	location ~ [^/]\.php(/|$) {
 		try_files $uri =404;
-		fastcgi_pass wordpress:9000
+		fastcgi_pass wordpress:9000;
 		include fastcgi_params;
 		fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
 	}
