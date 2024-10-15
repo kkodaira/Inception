e452635bf73e647fb59a89a82c1affb2f378b273
Kai Kodaira <kkodaira@c2r8s1.42tokyo.jp>
2024-10-15 19:56:55 +0900

    test

--- src/docker/nginx/files/setup.sh
+++ src/docker/nginx/files/setup.sh
@@ -2,14 +2,14 @@
 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
 -keyout /etc/ssl/private/nginx-selfsigned.key \
--out $CERTS_ -subj "/C=JP/L=TK/O=42/OU=student/CN=kkodaira.42.jp" \
+-out $CERTS_ -subj "/C=JP/L=TK/O=42/OU=student/CN=kkodaira.42.jp"
 
 echo "
 server { #https用の設定
 	listen 443 ssl;
 	listen [::]:443 ssl;
 	
-	#server_name $DOMAIN_NAME www.$DOMAIN_NAME;
+	server_name $DOMAIN_NAME www.$DOMAIN_NAME; #変えてみる
 	
 	ssl_certificate $CERTS_;
 	ssl_certificate_key /etc/ssl/private/nginx-selfsigned.key;" \
